60555cd085f6f0728e83a15ff9974fdc7cfd68fb
mahmoud kamel <mahmoudkamel267x@gmail.com>
2026-04-25 11:26:36 +0300

    feat: Add Windows runner and Flutter integration
    - Introduced a new Windows runner for the Flutter application, including necessary CMake configuration files.
    - Added resource files for application icon and version information.
    - Implemented core window management functionality with DPI awareness and theme support.
    - Included generated plugin registration files to facilitate plugin integration.
    - Added utility functions for command line argument handling and console attachment.
    - Introduced splash images for application branding.

---
+++ splash_screen_helper.dart
@@ -0,0 +1,26 @@
+
+// 1- add package flutter_launcher_icons in pubspec.yaml
+// icons app logo
+// 2- app IOS & Android:
+// # size 1024x1024
+// # PNG
+// # no alpha (no transparency)
+// # no rounded corners
+// # example:
+// assets/icons/app_icon.png
+
+// write in pubspec.yaml
+// dev_dependencies:
+//   flutter_launcher_icons: ^0.13.1
+
+// flutter_icons:
+//   android: true
+//   ios: true
+//   image_path: "assets/icons/app_icon.png"
+//   adaptive_icon_background: "#5F33E1"
+//   adaptive_icon_foreground: "assets/icons/app_icon_logo.png"
+//   remove_alpha_ios: true
+
+// final run this
+// flutter pub get
+// dart run flutter_launcher_icons
